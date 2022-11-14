3505eb608907874ed9c12e6f83f5caf2fcee033f
Pooja-sisodia <poojasisodia406@gmail.com>
2022-11-14 13:34:42 +0530

    adding authorModel  blogModel and router file codes

---
+++ src/model/blogModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema( {
+title:{ 
+    type: String,
+    required: true
+
+},
+body:{ 
+    type: String,
+    required: true,
+},
+authorId:{ 
+    type: String,
+    required: true,
+    ref:'authorModel'
+},
+tags:{ 
+    type: [String],
+
+},
+category:{ 
+    type: String,
+    required: true,
+},
+subcategory:{ 
+    type: [String]
+    
+},
+createdAt:{ 
+    type: Date
+},
+updatedAt:{ 
+    type: Date
+},
+deletedAt:{ 
+    type: Boolean,
+
+},
+isDeleted:{ 
+    type: Boolean,
+    default: false,
+},
+publishedAt:{ 
+    type: Boolean
+},
+isPublished:{ 
+    type: Boolean,
+    default: false
+}
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Blog', blogSchema)
+
